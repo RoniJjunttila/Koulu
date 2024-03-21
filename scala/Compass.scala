4929fd6a048aa0b9c98be440dd2606fb5cf9bf84
RoniJjunttila <127955066+RoniJjunttila@users.noreply.github.com>
2024-03-21 19:07:15 +0200

    Update Compass.scala

--- scala/Compass.scala
+++ scala/Compass.scala
@@ -5,4 +5,4 @@ object Compass extends App {
   val input = readLine()
   val values = Map("N" -> "North", "S" -> "South", "W" -> "West", "E" -> "East")
   println("You are going to " + values(input))
-}
+} 
